c150477772ae263ee9a35eae2e1b03a3d6384755
ccckmit <ccckmit@gmail.com>
2019-06-13 12:00:39 +0800

    reorganize doc

--- src/pub6.js
+++ src/pub6.js
@@ -7,39 +7,41 @@ const JSOX = require('jsox')
 
 let options = {toHtml:true, toPdf:false}
 
+async function handler(type, fpath, stack) {
+  var plugins = null
+  switch (type) {
+    case 'folder': 
+      await folderVisit(fpath, stack, handler)
+      break
+    case 'file':
+      plugins = stack[stack.length-1]
+      await md6.convertFile(fpath, options, plugins)
+      break
+    default:
+      throw Error('Error: convertAll: dirWalk' + type + ' not found !')
+  }
+}
+
+async function folderVisit(fpath, stack, handler) {
+  let parent = (stack.length > 0) ? stack[stack.length-1] : {}
+  let sidebar = await fs6.readText(path.join(fpath, '_sidebar.md'))
+  // console.log('sidebar=%s', sidebar)
+  let header  = await fs6.readText(path.join(fpath, '_header.md'))
+  let footer  = await fs6.readText(path.join(fpath, '_footer.md'))
+  let json6   = await fs6.readText(path.join(fpath, '_meta.json6'))
+  let meta    = JSOX.parse(json6||'{}')
+  meta = Object.assign(meta, parent.meta||{})
+  console.log('meta=%j', meta)
+  plugins = uu6.defaults({meta, sidebar, header, footer}, parent)
+  stack.push({meta, sidebar, header, footer})
+  await fs6.dirWalk(fpath, stack, handler)
+  stack.pop()
+}
+
 M.convertAll = async function(root)  {
   try {
     let stack = []
-    // await folderVisit(root)
-    fs6.dirWalk(root, stack, async function handler(type, fpath, stack) {
-      var plugins = null
-      switch (type) {
-        case 'folder': 
-          let parent = (stack.length > 0) ? stack[stack.length-1] : {}
-          let sidebar = await fs6.readText(path.join(fpath, '_sidebar.md'))
-          // console.log('sidebar=%s', sidebar)
-          let header  = await fs6.readText(path.join(fpath, '_header.md'))
-          let footer  = await fs6.readText(path.join(fpath, '_footer.md'))
-          let json6   = await fs6.readText(path.join(fpath, '_meta.json6'))
-          let meta    = JSOX.parse(json6||'{}')
-          meta = Object.assign(meta, parent.meta||{})
-          console.log('meta=%j', meta)
-          plugins = uu6.defaults({meta, sidebar, header, footer}, parent)
-          // console.log('  plugins=%j', plugins)
-          stack.push({meta, sidebar, header, footer})
-          await fs6.dirWalk(fpath, stack, handler)
-          stack.pop()
-          break
-        case 'file':
-          // console.log('file=', fpath)
-          plugins = stack[stack.length-1]
-          // plugins = stack[stack.length-1]
-          // console.log('  stack=%j', stack)
-          await md6.convertFile(fpath, options, plugins)
-          break
-        default:
-          throw Error('Error: convertAll: dirWalk' + type + ' not found !')
-      }
-    })
+    await folderVisit(root, stack, handler)
+    // fs6.dirWalk(root, stack, handler)
   } catch (error) { console.log('error=', error) }
 }
